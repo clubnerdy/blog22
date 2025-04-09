0313d4b803fbe1d5dd2586558a77fb55e827c56d
SeoHoejeong <seohoejeong@gmail.com>
2025-04-09 12:34:52 +0900

    ãã좋아요 완료

--- src/main/java/shop/mtcoding/blog/love/LoveService.java
+++ src/main/java/shop/mtcoding/blog/love/LoveService.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.blog.love;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +8,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoveService {
     private final LoveRepository loveRepository;
+
+    @Transactional
+    public LoveResponse.SaveDTO 좋아요(LoveRequest.SaveDTO reqDTO, Integer sessionUserId) {
+        Love lovePS = loveRepository.save(reqDTO.toEntity(sessionUserId));
+        Long loveCount = loveRepository.findByBoardId(reqDTO.getBoardId());
+        return new LoveResponse.SaveDTO(lovePS.getId(), loveCount.intValue());
+    }
+
+    @Transactional
+    public LoveResponse.DeleteDTO 좋아요취소(Integer id) {
+        Love lovePS = loveRepository.findById(id);
+        if (lovePS == null) throw new RuntimeException("좋아요를 안했는데 취소를 어캐해");
+
+        Integer boardId = lovePS.getBoard().getId();
+
+        Long loveCount = loveRepository.findByBoardId(boardId);
+        return new LoveResponse.DeleteDTO(loveCount.intValue());
+    }
 }
